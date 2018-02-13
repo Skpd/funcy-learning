8958f6ea1f7e97bc9e962c482de78da0d8d49655
k1998 <filippova.alina@gmail.com>
2018-02-12 18:56:47 -0800

    edits from skpd for tests

--- grabber.py
+++ grabber.py
@@ -20,16 +20,21 @@ def main():
         for x in range(args['from'], args['to'] + 1):
             player_id = "http://www.roswar.ru/player/{}/".format(x)
             r = requests.get(player_id)
-            soup = BeautifulSoup(r.text, "html.parser")
-            a = soup.find('span', class_="user")
-            if a is None:
-                print(x, 'not found')
-            else:
-                alignment = a.find('i')['class'][0]
-                name = a.select_one('a[href^="/player"]').get_text().strip()
-                level = a.find('span').get_text().strip('[]')
-                resultwriter.writerow([x, name, level, alignment])
-                print(x, 'done')
+            result = parse_html(html=r.text)
+            if 'name' in result and 'level' in result and 'alignment' in result:
+                resultwriter.writerow([x, result['name'], result['level'], result['alignment']])
+            print(x, 'done')
+
+
+def parse_html(html: str):
+    out = {}
+    soup = BeautifulSoup(html, "html.parser")
+    a = soup.find('span', class_="user")
+    if a is not None:
+        out['alignment'] = a.find('i')['class'][0]
+        out['name'] = a.select_one('a[href^="/player"]').get_text().strip()
+        out['level'] = a.find('span').get_text().strip('[]')
+    return out
 
 
 if __name__ == '__main__':
